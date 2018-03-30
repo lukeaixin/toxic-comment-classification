22e00e3d2a1f8963732c879f7627ec3851d86845
Xin Ai <lukeaixin@gmail.com>
2018-03-30 09:45:03 -0700

    formatting

--- module/get_column_names.py
+++ module/get_column_names.py
@@ -5,9 +5,9 @@ data_file = sys.argv[1]
 name_file = sys.argv[2]
 
 with open(data_file, 'r') as rfile:
-	reader = csv.reader(rfile)
-	names = next(reader)
+    reader = csv.reader(rfile)
+    names = next(reader)
 
 with open(name_file, 'w') as wfile:
-	for name in names:
-		wfile.write(name + '\n')
+    for name in names:
+        wfile.write(name + '\n')
