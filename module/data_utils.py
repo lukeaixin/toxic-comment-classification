03408767e942b243e51c60df67748a481c3b093a
Xin Ai <lukeaixin@gmail.com>
2018-03-31 17:24:11 -0700

    text preprocessing

--- module/data_utils.py
+++ module/data_utils.py
@@ -2,5 +2,5 @@ import numpy as np
 import pandas as pd
 
 def load_data(file, x_columns, y_column):
-	data = pd.read_csv(file, header=0)
-	return np.array(data.ix[:, x_columns]), np.array(data.ix[:, y_column])
+    data = pd.read_csv(file, header=0)
+    return np.array(data.ix[:, x_columns]), np.array(data.ix[:, y_column])
--- module/text_utils.py
+++ module/text_utils.py
@@ -1,10 +1,48 @@
 import re
+from nltk import pos_tag
+from nltk.tokenize import sent_tokenize
+from nltk.corpus import wordnet
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 def normalize(txt):
-	txt = txt.strip().lower()
-	txt = re.sub(r'[^\w\s]+', ' ', txt)
-	txt = re.sub(r'_+', ' ', txt)
-	txt = re.sub(r'\d+', ' ', txt)
-	txt = ' '.join(txt.split())
-	return txt
+    txt = txt.strip().lower()
+    txt = re.sub(r'[^\w\s]+', ' ', txt)
+    txt = re.sub(r'_+', ' ', txt)
+    txt = re.sub(r'\d+', ' ', txt)
+    return txt.split()
 
+"""
+Convert treebank pos tags to wordnet pos tags,
+in compliance with wordnet lemmatization.
+"""
+def treebank_to_wordnet(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN # default pos in wordnet lemmatization
+
+def lemmatize(words):
+    lemmatizer = WordNetLemmatizer()
+    word_pos = pos_tag(words)
+    return [lemmatizer.lemmatize(word, treebank_to_wordnet(pos)) for word, pos in word_pos]
+
+def remove_stopwords(words):
+    sw = set(stopwords.words('english'))
+    return [w for w in words if w not in sw]
+
+def prep_text(txt):
+    sents = sent_tokenize(txt)
+    prep_words = []
+    for sent in sents:
+        words = normalize(sent)
+        words = lemmatize(words)
+        words = remove_stopwords(words)
+        prep_words += words
+    return ' '.join(prep_words)
