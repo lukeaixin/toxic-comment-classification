725c262dfeb297f9a13521aa4fd710fa8d54558d
Xin Ai <lukeaixin@gmail.com>
2018-04-09 21:07:46 -0700

    round up running time

--- module/utils.py
+++ module/utils.py
@@ -1,5 +1,5 @@
 def seconds_to_str(seconds):
-    seconds = int(seconds)
+    seconds = int(seconds) + 1
     minutes, secs = divmod(seconds, 60)
     hours, mins = divmod(minutes, 60)
     days, hrs = divmod(hours, 24)
